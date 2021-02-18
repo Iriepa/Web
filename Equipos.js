d5a909db99df6b9e7d80092b8a1cf24480afdbaa
Iriepa <davidiriepa@gmail.com>
2021-02-18 10:22:49 +0100

    final version

---
+++ Equipos.js
@@ -0,0 +1,336 @@
+ var arrayEquipos =[
+
+ { //0
+ 	nombre:'DG',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FDK-FullonDark.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//1
+ 	nombre:'GEG',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592589327622_Gen.GGEN-01-FullonDark.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//2
+ 	nombre:'XTG',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592589284897_DRXDRX-01-FullonDark.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//3
+ 	nombre:'DAG',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FKT-FullonDark.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//4
+ 	nombre:'TG',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592589079394_T1T1-01-FullonDark.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//5
+ 	nombre:'ALG',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FLSB2-FullonDark.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//6
+ 	nombre:'AFG',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FAF-FullonDark.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//7
+ 	nombre:'SG',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FNSFullonDark.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//8
+ 	nombre:'MAD',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592591395339_MadLionsMAD-01-FullonDark.png" style="vertical-align: middle"',
+ 	fav:true
+ },
+ {//9
+ 	nombre:'XL',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FExcel_FullColor2.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//10
+ 	nombre:'S04',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FSchalke04_FullColor.png" style="vertical-align: middle"',
+ 	fav:true
+ },
+ {//11
+ 	nombre:'SK',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FSK_FullColor.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//12
+ 	nombre:'G2',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FG2_FullColor2.png" style="vertical-align: middle"',
+ 	fav:true
+ },
+ {//13
+ 	nombre:'MIS',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592591419157_MisfitsMSF-01-FullonDark.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//14
+ 	nombre:'EG',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592590374862_EvilGeniusesEG-01-FullonDark.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//15
+ 	nombre:'100',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592590000927_100Thieves100-01-FullonDark.png" style="vertical-align: middle"',
+ 	fav:true
+ },
+ {//16
+ 	nombre:'GG',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592590586919_GoldenGuardiansGGS-01-FullonDark.png" style="vertical-align: middle"',
+ 	fav:false
+ },
+ {//17
+ 	nombre:'C9',
+ 	logo:'<img src="https://am-a.akamaihd.net/image/?resize=36:&amp;f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1592590156110_Cloud9C9-01-FullonDark.png" style="vertical-align: middle"',
+ 	fav:true
+ }
+
+ ];
+
+ var arrayPartidas =[
+
+ {//0
+ 	equipo1: arrayEquipos[0],
+ 	equipo1puntos:150,
+ 	equipo2: arrayEquipos[1],
+ 	equipo2puntos: 200
+ },
+ {//1
+ 	equipo1: arrayEquipos[2],
+ 	equipo1puntos: 450,
+ 	equipo2: arrayEquipos[3],
+ 	equipo2puntos: 240
+ },
+ {//2
+ 	equipo1: arrayEquipos[4],
+ 	equipo1puntos: 150,
+ 	equipo2: arrayEquipos[5],
+ 	equipo2puntos: 220
+ },
+ {//3
+ 	equipo1: arrayEquipos[6],
+ 	equipo1puntos: 350,
+ 	equipo2: arrayEquipos[7],
+ 	equipo2puntos: 120
+ },
+ {//4
+ 	equipo1: arrayEquipos[8],
+ 	equipo1puntos: 430,
+ 	equipo2: arrayEquipos[9],
+ 	equipo2puntos: 210
+ },
+ {//5
+ 	equipo1: arrayEquipos[10],
+ 	equipo1puntos: 430,
+ 	equipo2: arrayEquipos[11],
+ 	equipo2puntos: 120
+ },
+ {//6
+ 	equipo1: arrayEquipos[12],
+ 	equipo1puntos: 430,
+ 	equipo2: arrayEquipos[13],
+ 	equipo2puntos: 540
+ },
+ {//7
+ 	equipo1: arrayEquipos[14],
+ 	equipo1puntos: 430,
+ 	equipo2: arrayEquipos[15],
+ 	equipo2puntos: 540
+ },
+ {//8
+ 	equipo1: arrayEquipos[16],
+ 	equipo1puntos: 430,
+ 	equipo2: arrayEquipos[17],
+ 	equipo2puntos: 540
+ },
+ {//9
+ 	equipo1: arrayEquipos[17],
+ 	equipo1puntos: 630,
+ 	equipo2: arrayEquipos[3],
+ 	equipo2puntos: 540
+ },
+ {//10
+ 	equipo1: arrayEquipos[3],
+ 	equipo1puntos: 430,
+ 	equipo2: arrayEquipos[5],
+ 	equipo2puntos: 540
+ },
+ {//11
+ 	equipo1: arrayEquipos[2],
+ 	equipo1puntos: 430,
+ 	equipo2: arrayEquipos[13],
+ 	equipo2puntos: 540
+ },
+ {//12
+ 	equipo1: arrayEquipos[11],
+ 	equipo1puntos: 430,
+ 	equipo2: arrayEquipos[7],
+ 	equipo2puntos: 540
+ },
+ {//13
+ 	equipo1: arrayEquipos[5],
+ 	equipo1puntos: 430,
+ 	equipo2: arrayEquipos[2],
+ 	equipo2puntos: 540
+ }
+
+
+ ];
+
+
+
+ ultimapartida=0;
+ function cargarEquipos(){
+
+ 	for (var i = 0; i<7; i++){
+ 		document.getElementById('partida' + (i+1)+'.1').innerHTML = arrayPartidas[i].equipo1.logo + '/> ' + arrayPartidas[i].equipo1.nombre;
+ 		document.getElementById('partida' + (i+1)+'.2').innerHTML = arrayPartidas[i].equipo2.logo + '/> ' + arrayPartidas[i].equipo2.nombre;
+ 		ultimapartida=i;
+ 	}
+ 	
+ 	cargarFavoritos();
+
+
+ 	/*document.getElementById("videos").innerHTML= 
+ 	'<iframe class="video1" width="400" height="320" src="https://www.youtube.com/embed/1cTyJdYxDd8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+ 	+'<iframe class="video2" width="400" height="320" src="https://www.youtube.com/embed/vrhJTAlpoCI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+ 	+'<iframe class="video3" width="400" height="320" src="https://www.youtube.com/embed/D1W2BWW0ibQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+ 	+'<iframe class="video4" width="400" height="320" src="https://www.youtube.com/embed/uw80Uk5m-mU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+ 	+'<iframe class="video5" width="400" height="320" src="https://www.youtube.com/embed/Gd1QKWuERW0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+ 	+'<iframe class="video6" width="400" height="320" src="https://www.youtube.com/embed/2-J19S3FpnI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+ */}
+
+ function flechaD(){
+ 	posicion=1;
+ 	rep= ultimapartida+7;
+ 	if (ultimapartida<7) {
+ 		for (var i = ultimapartida+1; i<=rep; i++){
+ 			document.getElementById('partida' + posicion+'.1').innerHTML = arrayPartidas[i].equipo1.logo + '/> ' + arrayPartidas[i].equipo1.nombre;
+ 			document.getElementById('partida' + posicion+'.2').innerHTML = arrayPartidas[i].equipo2.logo + '/> ' + arrayPartidas[i].equipo2.nombre;
+ 			ultimapartida=i;
+ 			posicion++;
+ 		}
+ 	}
+ }
+
+ function flechaI(){
+
+ 	if (ultimapartida>7) {
+ 		for (var i = 0; i<7; i++){
+ 			document.getElementById('partida' + (i+1)+'.1').innerHTML = arrayPartidas[i].equipo1.logo + '/> ' + arrayPartidas[i].equipo1.nombre;
+ 			document.getElementById('partida' + (i+1)+'.2').innerHTML = arrayPartidas[i].equipo2.logo + '/> ' + arrayPartidas[i].equipo2.nombre;
+ 			ultimapartida=i;
+ 		}
+ 	}
+ }
+
+
+ function cargarFavoritos(){
+ 	document.getElementById("busquedas").innerHTML =
+ 	'<div class="titulo" id="titulofav">Favoritos</div>'
+ 	+'<div class="favorito" id="fav1" onclick="cargarGanadorPerdedorFav(4)"></div>'
+ 	+'<div class="favorito" id="fav2" onclick="cargarGanadorPerdedorFav(5)"></div>'
+ 	+'<div class="favorito" id="fav3" onclick="cargarGanadorPerdedorFav(6)"></div>'
+ 	+'<div class="favorito" id="fav4" onclick="cargarGanadorPerdedorFav(7)"></div>'
+ 	+'<div class="favorito" id="fav5" onclick="cargarGanadorPerdedorFav(8)"></div>'
+
+ 	contador=1;
+ 	for (var i = 0; i<arrayEquipos.length; i++) {
+ 		if (arrayEquipos[i].fav==true) {
+ 			document.getElementById('fav'+contador).innerHTML = arrayEquipos[i].logo+'/> '+arrayEquipos[i].nombre; 
+ 			contador++;
+
+ 		}
+ 	}
+ }
+
+ function cargarGanadorPerdedor(partido){
+ 	if (ultimapartida<=6) {
+ 		for (var i =0; i<ultimapartida; i++) {
+ 			if (arrayPartidas[partido].equipo1puntos>arrayPartidas[partido].equipo2puntos) {
+ 				document.getElementById('resultado').innerHTML = arrayPartidas[partido].equipo1.logo + 'class="imagenes"/> '+ arrayPartidas[partido].equipo1.nombre+' <i class="gana"> Ganador </i> '+ arrayPartidas[partido].equipo2.logo + 'class="imagenes"/> ' + arrayPartidas[partido].equipo2.nombre + ' <i class="pierde"> Perdedor </i> <p class="pulsador"><i>pulsa aqui para quitar este resultado </i></p> ';
+ 			}else{
+ 				document.getElementById('resultado').innerHTML = arrayPartidas[partido].equipo1.logo + 'class="imagenes"/> ' +  arrayPartidas[partido].equipo1.nombre+' <i class="pierde"> Perdedor </i>  '+ arrayPartidas[partido].equipo2.logo + 'class="imagenes"/> ' + arrayPartidas[partido].equipo2.nombre + ' <i class="gana"> Ganador </i> <p class="pulsador"><i>pulsa aqui para quitar este resultado </i></p> ';
+ 			}
+ 		}
+ 	}else{
+ 		partido= partido+7;
+ 		for (var i =0; i<arrayPartidas.length; i++) {
+ 			if (arrayPartidas[partido].equipo1puntos>arrayPartidas[partido].equipo2puntos) {
+ 				document.getElementById('resultado').innerHTML = arrayPartidas[partido].equipo1.logo + 'class="imagenes"/> '+ arrayPartidas[partido].equipo1.nombre+' <i class="gana"> Ganador </i> '+ arrayPartidas[partido].equipo2.logo + 'class="imagenes"/> ' + arrayPartidas[partido].equipo2.nombre + ' <i class="pierde"> Perdedor </i> <p class="pulsador"><i>pulsa aqui para quitar este resultado </i></p> ';
+ 			}else{
+ 				document.getElementById('resultado').innerHTML = arrayPartidas[partido].equipo1.logo + 'class="imagenes"/> ' +  arrayPartidas[partido].equipo1.nombre+' <i class="pierde"> Perdedor </i>  '+ arrayPartidas[partido].equipo2.logo + 'class="imagenes"/> ' + arrayPartidas[partido].equipo2.nombre + ' <i class="gana"> Ganador </i> <p class="pulsador"><i>pulsa aqui para quitar este resultado </i></p> ';
+ 			}
+ 		}
+ 	}
+ }
+
+ function quitarGanadorPerdedor(){
+ 	document.getElementById('resultado').innerHTML ="" ;
+ }
+
+ function cargarGanadorPerdedorFav(partido){
+ 	for (var i =0; i<arrayPartidas.length; i++) {
+ 		if (arrayPartidas[partido].equipo1puntos>arrayPartidas[partido].equipo2puntos) {
+ 			document.getElementById('resultado').innerHTML = arrayPartidas[partido].equipo1.logo + 'class="imagenes"/> '+ arrayPartidas[partido].equipo1.nombre+' <i class="gana"> Ganador </i> '+ arrayPartidas[partido].equipo2.logo + 'class="imagenes"/> ' + arrayPartidas[partido].equipo2.nombre + ' <i class="pierde"> Perdedor </i> <p class="pulsador"><i>pulsa aqui para quitar este resultado </i></p>';
+ 		}else{
+ 			document.getElementById('resultado').innerHTML = arrayPartidas[partido].equipo1.logo + 'class="imagenes"/> ' +  arrayPartidas[partido].equipo1.nombre+' <i class="pierde"> Perdedor </i>  '+ arrayPartidas[partido].equipo2.logo + 'class="imagenes"/> ' + arrayPartidas[partido].equipo2.nombre + ' <i class="gana"> Ganador </i> <p class="pulsador"><i>pulsa aqui para quitar este resultado </i></p> ';
+ 		}
+ 	}
+ }
+
+
+inputSearch = document.getElementById("input-search");
+ function buscador(){
+ 	filtro = inputSearch.value.toUpperCase();
+
+
+	//Recorremos los elementos
+	j=1;
+	arrayGuardarEquipos=[]; 
+	for (var i = 0; i < arrayEquipos.length; i++) {
+		if (filtro=="") {
+			cargarFavoritos();
+		}else{
+			posicion=0;
+			for (var i = 0; i < arrayEquipos.length; i++) {
+				if (arrayEquipos[i].nombre.includes(filtro)) {
+
+					arrayGuardarEquipos[posicion] = arrayEquipos[i].logo + '/> ' + arrayEquipos[i].nombre;
+					posicion++;
+				}
+			}
+
+			
+		}//ELSE	
+	}
+
+	if(filtro!=""){
+
+		var contenedor = "";
+		for (var i = 0; i < arrayGuardarEquipos.length; i++) {
+			contenedor+='<div class="favorito" id="fav' +(i+1)+ '">' + '</div>';
+		}
+
+		document.getElementById("busquedas").innerHTML = contenedor;
+
+		for (var i = 0; i < arrayGuardarEquipos.length; i++) {
+			document.getElementById("fav"+(i+1)).innerHTML = arrayGuardarEquipos[i];
+		}
+	}
+	
+}
+
+
+
